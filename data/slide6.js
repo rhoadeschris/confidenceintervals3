904ae538ffddef94f9fde3f1223805a6690b7876
rhoadeschris <54925442+rhoadeschris@users.noreply.github.com>
2023-01-12 17:17:39 -0600

    files

---
+++ data/slide6.js
@@ -0,0 +1,11 @@
+(function(){var loadHandler=window['sl_{B0E220B9-944D-41D9-8719-0A27B3E69B61}'];loadHandler&&loadHandler(5, '<div id="spr0_7eb48ae"><div id="spr1_7eb48ae" class="kern slide"><img id="img2_7eb48ae" src="data/img0.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_7eb48ae" class="kern slide"><div id="spr3_7eb48ae" style="left:88px;top:38.333px;"><div style="width:0px;"><span id="txt0_7eb48ae" data-width="216.250000" style="left:9.6px;top:37.34px;">Formula:</span></div></div><div id="spr4_7eb48ae" style="left:99.75px;top:191.25px;"><img id="img0_7eb48ae" src="data/img2.png" width="426.75" height="364.5" alt="𝑪𝑰=  𝒙 ± 𝒕 𝒄 ∗   𝑺 𝒙    𝒏   \
+ 𝑺 𝒙 =𝒔𝒕. 𝒅𝒆𝒗. 𝒐𝒇 𝒔𝒂𝒎𝒑𝒍𝒆 \
+   𝒙 =𝒎𝒆𝒂𝒏 𝒐𝒇 𝒔𝒂𝒎𝒑𝒍𝒆 \
+  𝒕 𝒄 =𝒕 𝒄𝒓𝒊𝒕𝒊𝒄𝒂𝒍 𝒗𝒂𝒍𝒖𝒆   \
+  𝒏=𝒔𝒂𝒎𝒑𝒍𝒆 𝒔𝒊𝒛𝒆"/></div><div id="spr5_7eb48ae" style="left:699.75px;top:87.75px;"><img id="img1_7eb48ae" src="data/img3.png" width="506.25" height="525" alt="𝑪𝒂𝒍𝒄𝒖𝒍𝒂𝒕𝒐𝒓\
+\
+MenuStatistics\
+Confidence Intervals t-interval\
+Select stats if you already know the mean and standard deviation.\
+Select data if you have typed data into a spreadsheet. \
+"/></div></div></div>', '{"s":[]}');})();
